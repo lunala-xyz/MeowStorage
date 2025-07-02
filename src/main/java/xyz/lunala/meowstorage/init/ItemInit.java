abef95c720f57ba5b7352b053644eab71a241f85
MineKID-LP <maximilian.schueller@hotmail.com>
2025-07-02 23:36:14 +0200

    remove example item

--- src/main/java/xyz/lunala/meowstorage/init/ItemInit.java
+++ src/main/java/xyz/lunala/meowstorage/init/ItemInit.java
@@ -2,7 +2,6 @@ package xyz.lunala.meowstorage.init;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,6 +13,4 @@ public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
     public static final RegistryObject<Item> BIG_CHEST_ITEM = ITEMS.register("big_chest", () -> new BlockItem(BIG_CHEST.get(), new Item.Properties()));
-
-    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder().alwaysEat().nutrition(1).saturationMod(2f).build())));
 }
