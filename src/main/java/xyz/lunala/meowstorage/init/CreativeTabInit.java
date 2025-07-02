57e9ecc8de57b352c9ee61920e192fb26abcde97
MineKID-LP <maximilian.schueller@hotmail.com>
2025-07-02 23:46:32 +0200

    Update creative tab title

--- src/main/java/xyz/lunala/meowstorage/init/CreativeTabInit.java
+++ src/main/java/xyz/lunala/meowstorage/init/CreativeTabInit.java
@@ -1,20 +1,19 @@
 package xyz.lunala.meowstorage.init;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 
 import static xyz.lunala.meowstorage.Meowstorage.MODID;
-import static xyz.lunala.meowstorage.init.ItemInit.EXAMPLE_ITEM;
 import static xyz.lunala.meowstorage.init.ItemInit.BIG_CHEST_ITEM;
 
 public class CreativeTabInit {
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder().withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> EXAMPLE_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
-        output.accept(EXAMPLE_ITEM.get());
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("meowstorage_tab", () -> CreativeModeTab.builder().title(Component.translatable("item_group.%s.creative_tab".formatted(MODID))).withTabsBefore(CreativeModeTabs.COMBAT).icon(() -> BIG_CHEST_ITEM.get().getDefaultInstance()).displayItems((parameters, output) -> {
         output.accept(BIG_CHEST_ITEM.get());
     }).build());
 }
