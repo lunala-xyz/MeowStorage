9270d842082dc5331e09617905ffee048642094f
YannickMyxe <shawn.froste.sf@gmail.com>
2025-07-09 23:01:20 +0200

    Added energy system

--- src/main/java/xyz/lunala/meowstorage/block/entity/BatteryBlockEntity.java
+++ src/main/java/xyz/lunala/meowstorage/block/entity/BatteryBlockEntity.java
@@ -1,12 +1,69 @@
 package xyz.lunala.meowstorage.block.entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.energy.EnergyStorage;
+import net.minecraftforge.energy.IEnergyStorage;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import xyz.lunala.meowstorage.init.BlockEntityInit;
+import xyz.lunala.meowstorage.util.AdaptedEnergyStorage;
 
 public class BatteryBlockEntity extends BlockEntity {
+    private final int capacity = 10_000;
+    private final int maxTransferRate = 1_000;
+    private final EnergyStorage energyStorage = createEnergyStorage();
+    private final LazyOptional<IEnergyStorage> energyHandler = LazyOptional.of(() -> new AdaptedEnergyStorage(energyStorage) {
+        @Override
+        public boolean canExtract() {
+            return true;
+        }
+
+        @Override
+        public boolean canReceive() {
+            return true;
+        }
+
+        @Override
+        public int extractEnergy(int maxExtract, boolean simulate) {
+            return super.extractEnergy(maxExtract, simulate);
+        }
+
+        @Override
+        public int receiveEnergy(int maxReceive, boolean simulate) {
+            return super.receiveEnergy(maxReceive, simulate);
+        }
+    });
+
     public BatteryBlockEntity(BlockPos pos, BlockState state) {
         super(BlockEntityInit.BATTERY_BLOCK.get(), pos, state);
+    }
+
+    public void tickServer() {
+        boolean powered = energyStorage.getEnergyStored() > 0;
+        if (powered != getBlockState().getValue(BlockStateProperties.POWERED)) {
+            assert level != null;
+            level.setBlockAndUpdate(worldPosition, getBlockState().setValue(BlockStateProperties.POWERED, powered));
+        }
+    }
+
+    private EnergyStorage createEnergyStorage() {
+        return new EnergyStorage(capacity, maxTransferRate, maxTransferRate);
+    }
+
+    @NotNull
+    @Override
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
+        if (cap == ForgeCapabilities.ENERGY) {
+            return energyHandler.cast();
+        } else {
+            return super.getCapability(cap, side);
+        }
     }
 }
