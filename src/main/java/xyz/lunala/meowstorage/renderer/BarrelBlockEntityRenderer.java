fa34a8f9f1d62adcb3d9374aac06337b6cbea90b
MineKID-LP <maximilian.schueller@hotmail.com>
2025-07-17 23:46:21 +0200

    Clean up in renderer
    This is totally required trust me

--- src/main/java/xyz/lunala/meowstorage/renderer/BarrelBlockEntityRenderer.java
+++ src/main/java/xyz/lunala/meowstorage/renderer/BarrelBlockEntityRenderer.java
@@ -1,35 +1,31 @@
 package xyz.lunala.meowstorage.renderer;
 
 import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.LightTexture;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
-import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
-import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
-import net.minecraft.client.renderer.entity.ItemRenderer;
-import net.minecraft.client.renderer.texture.OverlayTexture;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LightLayer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+
 import org.joml.Quaternionf;
-import xyz.lunala.meowstorage.Meowstorage;
+
 import xyz.lunala.meowstorage.block.barrels.MeowBarrelBase;
 import xyz.lunala.meowstorage.block.entity.MeowBarrelEntityBase;
 
 public class BarrelBlockEntityRenderer implements BlockEntityRenderer<MeowBarrelEntityBase> {
 
-    public BarrelBlockEntityRenderer(BlockEntityRendererProvider.Context pContext) {
-
-    }
+    public BarrelBlockEntityRenderer(BlockEntityRendererProvider.Context pContext) {}
 
     @Override
     public void render(MeowBarrelEntityBase pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
-
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
         ItemStack itemStack = pBlockEntity.getRenderStack();
 
